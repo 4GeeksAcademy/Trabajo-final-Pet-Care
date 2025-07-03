b160fcf4521759fe9c0d21dc66405a9a282f0461
Pamelavignolo <136765465+Pamelavignolo@users.noreply.github.com>
2025-07-03 15:37:19 +0000

    Agregue el modelo vacuna

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
 
 
 db = SQLAlchemy()
@@ -43,5 +44,23 @@ class Pet(db.Model):
             "foto": self.foto, # <-- ???
             "peso": self.peso
         }
+
+class Vacuna(db.Model):
+    __tablename__ = "vacunas"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(80), nullable=False)
+    descripcion: Mapped[str] = mapped_column(String(250))
+    fecha_aplicacion: Mapped[date] = mapped_column(nullable=True)
     
+    mascota_id: Mapped[int] = mapped_column(db.ForeignKey("pets.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            "fecha_aplicacion": self.fecha_aplicacion.isoformat() if self.fecha_aplicacion else None,
+            "mascota_id": self.mascota_id
+        }
+   
     
